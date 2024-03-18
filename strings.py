3f306def8c49ea5114cb6c7979430dfe763563dc
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-17 21:10:13 -0600

    practice

--- strings.py
+++ strings.py
@@ -1,18 +1,34 @@
-myStr = "Hello,  World"
+myStr = "Hello World"
+
+print('My name is ' + myStr)
+
+print(f'My name is {myStr}')
+
+print("My name is {0}".format(myStr))
+
 #print(dir(myStr))
-
-print(myStr.upper())
-print(myStr.lower())
-print(myStr.swapcase())
-print(myStr.capitalize())
-#metodos encadenados
-print(myStr.replace('Hello','bye' ).upper())
-
-print(myStr.count('l'))
-
-print(myStr.startswith('Hello'))
-
-print(myStr.endswith('World'))
-
-print(myStr.split('o'))
-
+# print(myStr.upper())
+# print(myStr.lower())
+# print(myStr.swapcase())
+# print(myStr.capitalize())
+# #metodos encadenados
+# print(myStr.replace('Hello','bye' ).upper())
+# print(myStr.count('l'))
+# print(myStr.startswith('Hello'))
+# print(myStr.endswith('World'))
+# print(myStr.split('o'))
+# print(myStr.find('o'))
+# print(myStr.find('e'))
+# print(myStr.isnumeric())
+# print(myStr.isalpha())
+# print(myStr[0])
+# print(myStr[1])
+# print(myStr[2])
+# print(myStr[3])
+# print(myStr[4])
+# print(myStr[5])
+# print(myStr[6])
+# print(myStr[7])
+# print(myStr[8])
+# print(myStr[9])
+# print(myStr[10])
