7c3d0ad17059baaf6c9e7a241dc2b2ee923ff44d
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-28 00:07:29 -0600

    numbers

---
+++ numbers-python.py
@@ -0,0 +1,23 @@
+#199
+#14.4
+
+#print(type(9))
+#print(type(10.5))
+
+#print(1+1)
+
+#print(3/2)
+
+#print(2**3)
+#para obtener los residuos
+#print(10 % 10)
+
+#operaciones matematicas
+
+#print(20 - 10  / 5 * 3 ** 2)
+
+age = input ("insert your age : ")
+
+new_age = int(age) + 10
+
+print(type(int(new_age)))
