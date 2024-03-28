24d87487e47b670fd38cad7333f8082a9978631d
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-28 00:07:39 -0600

    lists

---
+++ listas.py
@@ -0,0 +1,34 @@
+demo_list = [1, "hello", 1.23,True, [1 ,2, 3, 4]]
+
+color = ['blue', 'red', 'white']
+#con tupla se permite hacer una lista
+number_list = list((1,2,3,4))
+
+#r = list(range(0,101))
+
+#print(r)
+#print(number_list)
+#print(dir(color))
+#print(len(color))
+
+#print(color)
+#color[1] = 'gray'
+#print(color)
+
+#print(dir(color))
+
+#color.extend(['violet', 'yellow'])
+#color.extend(['green', 'black'])
+
+#color.insert(-1,'violet')
+#color.insert(len(color), 'violet')
+#print(color)
+#color.pop()
+#print(color)
+#color.remove('white')
+#print(color)
+
+#color.sort(reverse=True)
+#print(color.count('red'))
+
+
