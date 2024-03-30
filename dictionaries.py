e5a2effdbc38d59b304e2bc92c6fd48faf08677c
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-29 23:59:31 -0600

    basics

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+product = {
+    "name": "book",
+    "quantity": 12,
+    "price": 5.99,
+}
+
+person = {
+    "first name":"Jose",
+    "second name": "Manuel",
+    "edad": 20
+}
+
+
+
+otroProducto = [
+    {"name": 'book', 'price': 90000},
+    {"name": 'pen', 'price': 1090}
+]
+print(person.items())
+print(otroProducto)
