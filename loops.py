3630336fdfb7590889c98c1ee89cc7f2e9ef2be7
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-29 23:59:15 -0600

    basics

---
+++ loops.py
@@ -0,0 +1,27 @@
+foods = ['apples','bread', 'cheese','milk','almonds']
+
+# print(foods[0])
+# print(foods[1])
+# print(foods[2])
+# print(foods[3])
+# print(foods[4])
+
+# for food in foods:
+#     if food == 'cheese':
+#         # print('you just bought this')
+#         continue
+#     print(food)
+
+# for number in range(1,8):
+#     print(number)
+
+# for letter in "hello":
+#     print(letter)
+
+# count = 0
+# while count <= 10:
+#     print(count)
+#     count = count + 1
+
+
+
