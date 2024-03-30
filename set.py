7355ade6e3a619beae82b764fb3d010b59fa8ad7
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-29 23:59:37 -0600

    basics

---
+++ set.py
@@ -0,0 +1,13 @@
+colors = {'red','green','blue'}
+
+# print(colors)
+# print('red' in colors)
+
+# colors.add('gray')
+# print(colors)
+# colors.remove('red')
+# print(colors)
+
+# print(colors.clear())
+del colors
+print(colors)
