753179ef15563037e054d2375ff05643f70ec0e2
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-29 23:59:47 -0600

    basics

---
+++ funtions.py
@@ -0,0 +1,21 @@
+# def hello(name):
+#     print("hello " + name)
+
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+# hello('mannuel')
+
+# def add(numberOne,numberTwo):
+#     return numberOne + numberTwo
+# print(add(10,19))
+
+add = lambda numberOne , numberTwo: numberOne + numberTwo
+print(add(10,18))
