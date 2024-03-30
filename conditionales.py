0c6ac0d4326f905bd243045d6c9a8f2bf5dc4762
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-03-29 23:59:23 -0600

    basics

---
+++ conditionales.py
@@ -0,0 +1,29 @@
+# x = 10
+
+# if x < 5:
+#     print("es menor")
+# else:
+#     print("es mayor")
+
+# color = 'blue'
+
+# if color == 'red':
+#     print('the color its red')
+# elif color == 'yellow':
+#     print('el color es amarillo')
+# else:
+#     print('el color siempre fue azul')
+
+name = "Manuel"
+
+lastName = "Rivera"
+#condicionales anidadas
+if name == "Manuel":
+    if lastName == "Rivera":
+        print('tu eres Manuel Rivera')
+    else:
+        print('tu no eres manuel rivera')
+else:
+    print('tu quien eres?')
+
+
