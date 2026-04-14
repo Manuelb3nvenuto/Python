40009d3d59a4cbaedf1554dafddbb34f986988c1
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2026-04-14 13:03:27 -0600

    .

---
+++ Data Science/modulo 1/modulo 2/Control de flujoLección 2.1.py
@@ -0,0 +1,19 @@
+# Ejemplo básico
+edad = 17
+
+if edad >= 18:
+    print("Eres mayor de edad → puedes votar y comprar cerveza 🍺")
+else:
+    print("Eres menor → espera unos años 😅")
+
+# Con elif (else if) para múltiples condiciones
+nota = 85
+
+if nota >= 90:
+    print("Excelente! A+")
+elif nota >= 80:
+    print("Muy bien! B")
+elif nota >= 70:
+    print("Aprobado raspando C")
+else:
+    print("Reprobado... estudia más 💪")
