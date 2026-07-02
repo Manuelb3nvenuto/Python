3e5bf9b36ed36a3ff6ee7a7b0fb4bb738e959426
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2026-07-02 00:26:20 -0600

    Project #1
    The first project was about creating a band name generator, using strings, inputs

---
+++ Course/BandNameGenerator.py
@@ -0,0 +1,9 @@
+print("Welcome to the band generator, to start with it \n Tell us the name of the city you grew up in: ")
+
+city = input("") #in this part of the input line we can storage the info givin on the text that the input has
+
+print("Now can you tell us any name of a pet that you had: ")
+
+pet = input("")
+
+print("The name of the you band based on the answers given would be: "+ city + " " + pet)
